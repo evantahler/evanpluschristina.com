5b167f76f96a8e8d798795945b03daeb1d016069
Evan Tahler <evantahler@gmail.com>
2015-05-17 18:27:27 +0100

    add FAQ

---
+++ js/faq.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $('#faq_answer_1').hide();
+  $('#faq_answer_2').hide();
+  $('#faq_answer_3').hide();
+  $('#faq_answer_4').hide();
+  $('#faq_answer_5').hide();
+  $('#faq_answer_6').hide();
+});
